cc61e22637ec074d37cc46c1e1b4bbe7161765a1
Steve McLaughlin <stevejrmc@gmail.com>
2019-12-06 14:37:58 -0800

    Add route to handle request for listing--carousel/similar.

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const port = 3000;
+const Listing = require('../db').Listing;
 
 // Seed database
 require('../db/seed').seed();
@@ -11,7 +12,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 // TODO: Add necessary middleware
-// TODO: Handle request
+// TODO: Refactor request
+app.get('/:id', (req, res) => {
+  let listingID = req.params.id;
+  Listing.find({listingID: listingID}, (err, listing) => {
+    if (err) {
+      return res.status(404).end();
+    }
+    res.status(200).send(listing);
+  });
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
