0c10539f8d2c29ce016b6b6334f66fc42f293d92
Steven Contreras <SFContra@gmail.com>
2019-12-20 15:47:46 -0800

    Adjust for postgress
    concurrent

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const port = 3004;
 const cors = require('cors');
-const Listing = require('../db').Listing;
+const router = require('./router.js');
+
+const app = express();
+const port = 3004;
 
 app.use(cors());
 app.use(morgan('dev'));
@@ -12,15 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../dist'));
 
-app.get('/carousel-service/:id', (req, res) => {
-  let listingID = req.params.id;
-  Listing.find({listingID: listingID}, (err, listing) => {
-    if (err) {
-      return res.status(404).end();
-    }
-    res.status(200).send(listing[0]);
-  });
-});
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
---
+++ server/router.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const Listing = require('../db').Listing;
+
+router
+  .route('/carousel-service/:id')
+  .get((req, res) => {
+    let listingID = req.params.id;
+    Listing.find({listingID: listingID}, (err, listing) => {
+        if (err) {
+            console.error(err);
+        return res.status(404).end();
+        }
+        res.status(200).send(listing[0]);
+    });
+  });
+  
+  module.exports = router;
+
+// app.get('/carousel-service/:id', (req, res) => {
+//     let listingID = req.params.id;
+//     Listing.find({listingID: listingID}, (err, listing) => {
+//       if (err) {
+//         return res.status(404).end();
+//       }
+//       res.status(200).send(listing[0]);
+//     });
+//   });
